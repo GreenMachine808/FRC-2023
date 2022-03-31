32c952629aca4b17f11473dcc2448a353e1fc455
LiamKellogg <liampdk@gmail.com>
2022-03-30 17:49:19 -1000

    Changed the button of shooter from 2 to 1

--- src/main/java/frc/robot/DriveControls.java
+++ src/main/java/frc/robot/DriveControls.java
@@ -30,7 +30,7 @@ public class DriveControls {
   public double getYaw() { return joystick1.getRawAxis(0); }
   public double getElevatorAxis() { return joystick2.getRawAxis(1); }
 
-  public final JoystickButton shooter = new JoystickButton(joystick2, 2);
+  public final JoystickButton shooter = new JoystickButton(joystick2, 1);
 
   public final JoystickButton elevatorState = new JoystickButton(joystick2, 3);
   public final JoystickButton dropElevator0_0 = new JoystickButton(joystick2, 10);
